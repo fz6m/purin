e05291fff7261e5980aba5bdadbad4217a9ebfb0
fz6m <59400654+fz6m@users.noreply.github.com>
2023-11-20 00:46:26 +0800

    wip: add `web/{assets, constants}`

---
+++ packages/web/src/constants/index.ts
@@ -0,0 +1,8 @@
+export const TZ = 'Asia/Shanghai'
+
+export const APIS = {
+  lists: '/api/v1/lists',
+  tweets: '/api/v1/tweets',
+} as const
+
+export const DAY_LIMIT = 9
---
+++ packages/web/src/constants/metadata.ts
@@ -0,0 +1,7 @@
+export const meta = {
+  title: 'Purin X Explorer',
+  description: 'X(Twitter) list collector',
+  author: 'fz6m',
+  keywords: ['X', 'Twitter', 'list', 'collector', 'explorer'].join(','),
+  og_image: '/og.png',
+} as const
