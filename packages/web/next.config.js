9df37adba539995021adee79d86233d90a6b2882
fz6m <59400654+fz6m@users.noreply.github.com>
2023-11-20 03:00:25 +0800

    fix: prisma in monorepo

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,4 +1,15 @@
+// https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-monorepo
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
+}
 
 module.exports = nextConfig
