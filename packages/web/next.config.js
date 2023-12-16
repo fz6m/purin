cdc2f1cdbc4eff8f978ab1b60f48e9054c669726
fz6m <59400654+fz6m@users.noreply.github.com>
2023-12-16 19:20:20 +0800

    ci: why build failed

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,12 +1,10 @@
 // https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-monorepo
 const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
 
-const isDev = process.env.NODE_ENV === 'development'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { isServer }) => {
-    if (!isDev && isServer) {
+    if (isServer) {
       config.plugins = [...config.plugins, new PrismaPlugin()]
     }
 
